249f6592ef6cf62d1fa6515af1ba91f4f08a3cf7
Alberto Zuin <alberto@azns.it>
2014-02-28 07:52:11 +0100

    Changed redis server address from localhost to dynamic (must be declared in /etc/hosts)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://127.0.0.1:6379/12', :namespace => 'api' }
+  config.redis = { :url => 'redis://redis-server:6379/12', :namespace => 'api' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://127.0.0.1:6379/12', :namespace => 'api' }
+  config.redis = { :url => 'redis://redis-server:6379/12', :namespace => 'api' }
 end
