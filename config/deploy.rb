98a5fe1eb146dfec77a6f564f0d01ebf220fb3e9
Administrator <alberto@azns.it>
2014-05-01 18:00:40 +0200

    aggiunto deploy su host moyd

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :repo_url, 'git@git.azcloud.it:alberto/api-moyd-co.git'
 # set :log_level, :debug
 # set :pty, true
 
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/settings.local.yml config/sidekiq.yml config/sidekiq.yml config/initializers/sidekiq.rb}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -27,28 +27,14 @@ set :repo_url, 'git@git.azcloud.it:alberto/api-moyd-co.git'
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute "ln -nfs #{shared_path}/tmp #{release_path}/tmp"
-      execute "ln -nfs #{shared_path}/settings.local.yml #{release_path}/config/settings.local.yml"
-      execute "ln -nfs #{shared_path}/sidekiq.yml #{release_path}/config/sidekiq.yml"
-      execute "ln -nfs #{shared_path}/mongoid.yml #{release_path}/config/mongoid.yml"
-      execute "ln -nfs #{shared_path}/sidekiq.rb #{release_path}/config/initializers/sidekiq.rb"
-      #execute "ln -nfs #{shared_path}/newrelic.yml #{release_path}/config/newrelic.yml"
-      #execute :touch, "#{shared_path}/tmp/restart.txt"
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+#  desc "Prepare our symlinks" #7
+#  task :post_symlink do
+#      run "ln -nfs #{shared_path}/tmp #{release_path}/tmp"
+#      run "ln -nfs #{shared_path}/settings.local.yml #{release_path}/config/settings.local.yml"
+#      run "ln -nfs #{shared_path}/sidekiq.yml #{release_path}/config/sidekiq.yml"
+#      run "ln -nfs #{shared_path}/mongoid.yml #{release_path}/config/sidekiq.yml"
+#      run "ln -nfs #{shared_path}/sidekiq.rb #{release_path}/config/initializers/sidekiq.rb"
+#  end
 
   after :finishing, 'deploy:cleanup'
 
--- config/initializers/sidekiq.rb
+++
@@ -1,7 +0,0 @@
-Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://redis-server:6379/12', :namespace => 'api' }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://redis-server:6379/12', :namespace => 'api' }
-end
