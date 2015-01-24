b858e0200d1d8324f139c065b63761673404ff88
Administrator <alberto@azns.it>
2015-01-24 13:59:47 +0000

    Documentation in RAML

--- app/controllers/v1/dns_datas_controller.rb
+++ app/controllers/v1/dns_datas_controller.rb
@@ -85,55 +85,57 @@ class V1::DnsDatasController < ApplicationController
   end
 
   def update_from_check
-    check = Check.find(params[:id])
-    status = check.choose_status(params[:status])
-    status_change = false
-    if status == 'OK'
-      if check.hard_status
-        # if is OK do nothing, only reset counter
-        check.soft_count = 0
-        check.soft_status = true
-        check.hard_status = true
-      else
-        # if hard is in error
-        check.soft_count += 1
-        check.soft_status = true
-        # check if I have to enable this host
-        if check.soft_count >= check.soft_to_hard_to_enable
+    check = Check.where(id: params[:id]).first
+    unless check.nil?
+      status = check.choose_status(params[:status])
+      status_change = false
+      if status == 'OK'
+        if check.hard_status
+          # if is OK do nothing, only reset counter
+          check.soft_count = 0
+          check.soft_status = true
           check.hard_status = true
-          check.soft_count = 0
-          status_change = true
-        end
-      end
-    else
-      if check.hard_status
-        # if hard is ok
-        check.soft_count += 1
-        check.soft_status = false
-        # check if I have to enable this host
-        if check.soft_count >= check.soft_to_hard_to_disable
-          check.hard_status = false
-          check.soft_count = 0
-          status_change = true
+        else
+          # if hard is in error
+          check.soft_count += 1
+          check.soft_status = true
+          # check if I have to enable this host
+          if check.soft_count >= check.soft_to_hard_to_enable
+            check.hard_status = true
+            check.soft_count = 0
+            status_change = true
+          end
         end
       else
-        # if hard is error do nothing, only reset counter
-        check.soft_count = 0
-        check.soft_status = false
-        check.hard_status = false
+        if check.hard_status
+          # if hard is ok
+          check.soft_count += 1
+          check.soft_status = false
+          # check if I have to enable this host
+          if check.soft_count >= check.soft_to_hard_to_disable
+            check.hard_status = false
+            check.soft_count = 0
+            status_change = true
+          end
+        else
+          # if hard is error do nothing, only reset counter
+          check.soft_count = 0
+          check.soft_status = false
+          check.hard_status = false
+        end
       end
-    end
 
-    check.save
+      check.save
 
-    if status_change and not check.reports_only
-      check.records.each do |record|
-        record.operational = check.hard_status
-        record.save
-      end
-      if Settings.notify_changes_to_check == 'true'
-        Region.where(:has_check => true).each do |region|
-          region.update_check_server(check.id.to_s)
+      if status_change and not check.reports_only
+        check.records.each do |record|
+          record.operational = check.hard_status
+          record.save
+        end
+        if Settings.notify_changes_to_check == 'true'
+          Region.where(:has_check => true).each do |region|
+            region.update_check_server(check.id.to_s)
+          end
         end
       end
     end
